03cd961990201c4f2dd83bd54da25d808071a7c0
BenCoDev <benoit.cotte@essec.edu>
2023-08-08 22:20:28 +0200

    Get animation for dot complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import Image from 'next/image'
 
 import imessageAppIcon from './imessage.svg';
 import messengerAppIcon from './messenger.svg';
+import { AppIcon } from './components/AppIcon';
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
       <section className="flex w-[375px] h-[812px] bg-black p-4">
         <div className="flex self-end gap-8 items-center justify-center w-full h-[94px] bg-white/20 rounded-[40px]">
-          <Image
+          <AppIcon
             src={imessageAppIcon}
             alt="iMessage App Icon"
-            layout='contained'
+            items={[{content: 'Yo'}]}
           />
-          <Image
+          <AppIcon
             src={messengerAppIcon}
             alt="Messenger App Icon"
-            layout='contained'
           />
         </div>
       </section>
